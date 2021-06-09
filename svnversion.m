2444d8be1dfa4cdc8731dd0643a9e53c918bd2c3
cykhung <45437973+cykhung@users.noreply.github.com>
2021-06-09 16:16:00 -0400

    Sync up with skunk.

--- svnversion.m
+++ svnversion.m
@@ -1,11 +1,12 @@
-function svnversion(varargin)
+function varargout = svnversion(varargin)
 
 
 %% Get absolute path of svnversion.exe.
 svnver   = '1.8.5';
 switch svnver
 case '1.8.5'
-    svnexe = fullfile(fileparts(mfilename('fullpath')), 'svnversion.exe');
+    svnexe = fullfile(findroot('svn'), 'svn-win32-1.8.5', 'bin', ...
+        'svnversion.exe');
 otherwise
     error('Invalid svnver.');
 end
@@ -18,6 +19,17 @@ if nargout == 0
 end
 if status ~= 0
     error('status ~= 0');
+end
+
+
+%% Assign output arguments.
+switch nargout
+case 0
+    % Do nothing.
+case 1
+    varargout = {result};
+otherwise
+    error('Invalid number of output arguments.')
 end
 
 
